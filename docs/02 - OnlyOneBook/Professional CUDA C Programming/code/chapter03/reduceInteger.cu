1d3cec97815eb411dbe0cc9b06104208d5d73c09
LinXi <LinXi198913@163.com>
2025-02-18 15:11:04 +0800

    modified Cmake files

--- docs/02 - OnlyOneBook/Professional CUDA C Programming/code/chapter03/reduceInteger.cu
+++ docs/02 - OnlyOneBook/Professional CUDA C Programming/code/chapter03/reduceInteger.cu
@@ -11,15 +11,15 @@ int recursiveReduce(int* data, int const size) {
 
     if (size % 2 == 1) {
         for (int i = 0; i < stride; i++) {
-            data[i] += data[i + stride];
+            data[i] += data[i + stride]; 
         }
         data[0] += data[size - 1]; // add the last element to the first element
     } else {
         for (int i = 0; i < stride; i++) {
-            data[i] += data[i + stride];
+            data[i] += data[i + stride]; 
         }
     }
-    // recursive call
+    // call
     return recursiveReduce(data, stride);
 }
 
@@ -29,9 +29,9 @@ __global__ void warmup(int* g_idata, int* g_odata, unsigned int n) {
     // boundary check
     if (tid >= n)
         return;
-    // convert global data pointer to the in-place reduction in global memory
+    // convert global data pointer to the
     int* idata = g_idata + blockIdx.x * blockDim.x;
-     
+    // in-place reduction in global memory
     for (int stride = 1; stride < blockDim.x; stride *= 2) {
         if ((tid % (2 * stride)) == 0) {
             idata[tid] += idata[tid + stride];
@@ -39,7 +39,7 @@ __global__ void warmup(int* g_idata, int* g_odata, unsigned int n) {
         // synchronize within block
         __syncthreads();
     }
-    // write result for this block to global memory
+    // write result for this block to global mem
     if (tid == 0)
         g_odata[blockIdx.x] = idata[0];
 }
@@ -50,9 +50,9 @@ __global__ void reduceNeighbored(int* g_idata, int* g_odata, unsigned int n) {
     // boundary check
     if (tid >= n)
         return;
-    // convert global data pointer to the in-place reduction in global memory
+    // convert global data pointer to the
     int* idata = g_idata + blockIdx.x * blockDim.x;
-     
+    // in-place reduction in global memory
     for (int stride = 1; stride < blockDim.x; stride *= 2) {
         if ((tid % (2 * stride)) == 0) {
             idata[tid] += idata[tid + stride];
@@ -60,7 +60,7 @@ __global__ void reduceNeighbored(int* g_idata, int* g_odata, unsigned int n) {
         // synchronize within block
         __syncthreads();
     }
-    // write result for this block to global memory
+    // write result for this block to global mem
     if (tid == 0)
         g_odata[blockIdx.x] = idata[0];
 }
