bf4638f2f85ff3c81fdecc1499748ed07202cd48
LinXi <LinXi198913@163.com>
2025-07-09 01:39:09 +0800

    cpp Part.2

--- docs/02 - OnlyOneBook/Modern C++/code/OOP2/01_convertFunc/convertFunc.cpp
+++ docs/02 - OnlyOneBook/Modern C++/code/OOP2/01_convertFunc/convertFunc.cpp
@@ -2,35 +2,44 @@
 
 class Fraction {
     public:
-        // 使用explicit声明构造函数，防止隐式类型转换
-        // explicit Fraction(int num, int den = 1)		
+        // 函数1：使用explicit声明构造函数，防止隐式类型转换
+        // explicit Fraction(int num, int den = 1)
         //         : m_numerator(num), m_denominator(den) {}
-        Fraction(int num, int den = 1)		
-                : m_numerator(num), m_denominator(den) {}
-        
-        // 使用explicit声明类型转换运算符，防止隐式类型转换
-        explicit operator double() const {    		
-            return (double) (m_numerator * 1.0 / m_denominator);
-        }
+        Fraction(int num, int den = 1):
+            m_numerator(num), m_denominator(den) {}
 
-        double operator+(double d) const {
-            return (double) (m_numerator * 1.0 / m_denominator) + d;
-        }
-
+        // 函数2：重载运算符 +，用于 Fraction 对象之间的加法
         Fraction operator+(const Fraction &f) const {
             return Fraction(m_numerator + f.m_numerator, m_denominator + f.m_denominator);
         }
-    
+
+        // 函数3：使用隐式类型转换，会与重载运算符 + 冲突
+        // operator double() const {
+        //     return (double)(m_numerator * 1.0 / m_denominator);
+        // }
+
+        // 函数4：使用explicit声明类型转换运算符，防止隐式类型转换
+        explicit operator double() const {
+            return (double)(m_numerator * 1.0 / m_denominator);
+        }
+
+        // 函数5：重载运算符 +，用于 Fraction 对象与 double 类型的加法
+        double operator+(double d) const {
+            return (double)(m_numerator * 1.0 / m_denominator) + d;
+        }
+
     private:
-        int m_numerator;        
-        int m_denominator;      
-    };
+        int m_numerator;
+        int m_denominator;
+};
 
 int main() {
     Fraction f1(3, 5);
-    double d = f1 + 4; 	// 正确，显式类型转换
-    Fraction f2 = f1 + 4;
+    Fraction f2 = f1 + 4;       // 调用函数2，即重载的运算符 +，将 Fraction 对象与整数相加
+    // double f3 = f1 + 4;         // 调用函数3，即隐式类型转换，将 Fraction 对象转换为 double，编译歧义
+    double f4 = f1 + 4;         // 调用函数5，重载运算符 +，Fraction 对象与 double 类型直接相加
+
     printf("f2 = %f\n", (double)f2);
-    printf("%f\n", d);
+    printf("f4 = %f\n", f4);
     return 0;
 }
