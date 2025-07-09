fee81ed4d615440a1ce0ec50fe19fc1dfa1cb694
LinXi <LinXi198913@163.com>
2025-07-09 21:51:32 +0800

    cpp Part.2

---
+++ docs/02 - OnlyOneBook/Modern C++/code/OOP2/09_Binding/Binding.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class Base {
+    public:
+        virtual void show() {
+            cout << "Base class show function" << endl;
+        }
+        virtual ~Base() {
+            cout << "Base class destructor" << endl;
+        }
+};
+
+class Derived1: public Base {
+    public:
+        virtual void show() {
+            cout << "Derived1 class show function" << endl;
+        }
+};
+
+class Derived2: public Base {
+    public:
+        virtual void show() {
+            cout << "Derived2 class show function" << endl;
+        }
+};
+
+int main() {
+    Base*     b  = new Derived1();
+    Derived2* d2 = new Derived2();
+    Base*     b2 = d2;
+    Base* b3 = new Derived1()
+
+    b->show();              // 动态绑定，输出 Derived1 class show function
+    d2->show();             // 动态绑定，输出 Derived2 class show function
+    b2->show();             // 动态绑定，输出 Derived2 class show function
+    b3->Base::show();       // 静态绑定，输出 Base class show function
+
+    delete b;
+    delete d2;
+    delete b3;
+
+    return 0;
+}
