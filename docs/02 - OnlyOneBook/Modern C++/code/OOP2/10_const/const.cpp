ffbc63a5b2091302b03d68a961307bf473b65ccd
LinXi <LinXi198913@163.com>
2025-07-09 22:41:36 +0800

    cpp Part.2

--- docs/02 - OnlyOneBook/Modern C++/code/OOP2/10_const/const.cpp
+++ docs/02 - OnlyOneBook/Modern C++/code/OOP2/10_const/const.cpp
@@ -36,17 +36,18 @@ int main() {
 
     // 非常量对象可以调用非常量成员函数和常量成员函数
     obj.setValue(10);
-    cout << "Value from non-const object: " << obj.getValue() << endl;
 
-    // 常量对象只能调用常量成员函数
-    // constObj.setValue(20); // 错误：常量对象不能调用非常量成员函数
-    cout << "Value from const object: " << constObj.getValue() << endl;
+    // 非常量对象调用非常量成员函数，且优先调用非常量重载函数
+    obj.print();    // 输出: Non-const function: 10
 
     // 常量对象调用常量成员函数
     constObj.print();    // 输出: Const function: 0
 
-    // 非常量对象调用非常量成员函数
-    obj.print();    // 输出: Non-const function: 10
+    // 常量对象只能调用常量成员函数
+    // constObj.setValue(20); // 错误：常量对象不能调用非常量成员函数
+
+    cout << "Value from non-const object: " << obj.getValue() << endl;
+    cout << "Value from const object: " << constObj.getValue() << endl;
 
     return 0;
 }
